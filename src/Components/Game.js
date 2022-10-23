2fb03ced3b92b71f9af4665c89f4431ca39506eb
Harnick Khera <20884880+Hephyrius@users.noreply.github.com>
2022-10-23 12:09:14 +0100

    Update Game.js

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -19,13 +19,34 @@ export default function Game() {
   const [state, dispatch] = useContext(Context);
   const classes = GlobalStyles();
 
+  const handleCreateFarm = (e) => {
+    e.preventDefault();
+    createFarm(state.web3, 0, 0);
+  }
+
+  const handleHarvest = (e) => {
+    e.preventDefault();
+    var farm = usersFarm(state.web3, 0, 0)
+    if(farm != "0x0000000000000000000000000000000000000000"){
+     harvest(state.web3, farm);
+    }
+  }
+
+  const handlePlantTree = (e) => {
+    e.preventDefault();
+    var farm = usersFarm(state.web3, 0, 0)
+    if(farm != "0x0000000000000000000000000000000000000000"){
+     upgradeResource(state.web3, farm, 0);
+    }
+  }
+
   return (
     <div className="Game">
       <Card>
-        <Button variant="contained" color="primary" onClick={handleRewardClaim} className={classes.button}> Create Farm </Button>
-        <Button>Create Farm</Button>
-        <Button>Plant Tree</Button>
-        <Button>Harvest</Button>
+        <Button variant="contained" color="primary" onClick={handleCreateFarm} className={classes.button}> Create Farm </Button>
+        <Button variant="contained" color="primary" onClick={handleHarvest} className={classes.button}> Plant Tree </Button>
+        <Button variant="contained" color="primary" onClick={handlePlantTree} className={classes.button}> Harvest </Button>
+
       </Card>
     </div>
   );
